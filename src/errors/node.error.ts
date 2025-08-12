ed7f743007c6b6a66303cfa184ea148fe5015e9b
Benjamin Rosario <benjaminfl119@gmail.com>
2025-08-12 10:12:33 -0400

    fix build

--- src/errors/node.error.ts
+++ src/errors/node.error.ts
@@ -1,6 +1,6 @@
-import { HTTP_STATUS_BAD_REQUEST } from '@/constants/status-codes'
+import { PM2_CONNECTION_ERROR } from '@src/constants/error-types'
 import { BaseError } from './base.error'
-import { PM2_CONNECTION_ERROR } from '@/constants/error-types'
+import { HTTP_STATUS_BAD_REQUEST } from '@src/constants/status-codes'
 
 export class ConnectionError extends BaseError {
   constructor(message: string) {
